f5d60886b174b6a76de024445e599da5d7691eae
Zaidov012 <zaidov012@gmail.com>
2020-09-02 02:10:30 +0400

    Added some changes #1 (Windows, Edited VM's and Commands)

---
+++ NotesApp/ViewModel/LoginVM.cs
@@ -0,0 +1,31 @@
+﻿using NotesApp.Model;
+using NotesApp.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.ViewModel
+{
+    public class LoginVM
+    {
+        private User user;
+
+        public User User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+
+        public RegisterCommand RegisterCommand { get; set; }
+
+        public LoginCommand LoginCommand { get; set; }
+
+        public LoginVM()
+        {
+            RegisterCommand = new RegisterCommand(this);
+            LoginCommand = new LoginCommand(this);
+        }
+    }
+}
---
+++ NotesApp/ViewModel/NotesVM.cs
@@ -0,0 +1,61 @@
+﻿using NotesApp.Model;
+using NotesApp.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.ViewModel
+{
+    public class NotesVM
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
+        private Notebook selectedNotebook;
+        public Notebook SelectedNotebook
+        {
+            get { return selectedNotebook; }
+            set
+            {
+                selectedNotebook = value;
+                //TODO: get notes
+            }
+        }
+
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+        public NewNoteCommand NewNoteCommand { get; set; }
+
+        public NotesVM()
+        {
+            NewNotebookCommand = new NewNotebookCommand(this);
+            NewNoteCommand = new NewNoteCommand(this);
+        }
+
+        public void CreateNotebook()
+        {
+            Notebook newNotebook = new Notebook()
+            {
+                Name = "New notebook"
+            };
+
+            DatabaseHelper.Insert(newNotebook);
+        }
+
+        public void CreateNote(int notebookId)
+        {
+            Note newNote = new Note()
+            {
+                NotebookId = notebookId,
+                CreatedTime = DateTime.Now,
+                UpdatedTime = DateTime.Now,
+                Title = "New note"
+            };
+
+            DatabaseHelper.Insert(newNote);
+        }
+    }
+}
